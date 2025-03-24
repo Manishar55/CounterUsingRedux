c39d39cadd66601565fe706181846ccb7fffc45c
Manisha Rana <manisharana02005@gmail.com>
2025-03-25 00:25:52 +0530

    Created store for the slice

---
+++ src/redux/Store.jsx
@@ -0,0 +1,10 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import { CounterSlice } from "./slices/CounterSlice";
+
+//creating global store using configureStore function
+export const Store=configureStore({
+    reducer: {
+        counter: CounterSlice  //we defined a slice in teh reducer counter->reducer, counterSlice->slice
+    },
+})
